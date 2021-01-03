8843fad131c4ecf3942ca9561cf1367158ce3373
Dylan <ricedylan3@gmail.com>
2021-01-03 15:31:55 -0800

    Fixed Google Tests
    I'm not thrilled with the current results in VS test explorer namespace is empty this makes for a akward display but it's a good start.
    
    Doesn't seem to be a good solution to this problem Link: https://developercommunity.visualstudio.com/content/problem/663974/test-explorer-namespace-seems-to-not-be-working.html
    
    So I'll just deal for the time being.

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,8 +1,24 @@
 #include "gtest/gtest.h"
 
-TEST(blaTest, test1)
+namespace Snoodle
 {
-	EXPECT_EQ(1, 1);
+	TEST(ExampleTests, test1)
+	{
+		EXPECT_EQ(1, 1);
 
-	EXPECT_EQ(1, 1);
-}
+		EXPECT_EQ(17, 17);
+	}
+
+	TEST(ExampleTest2, test2)
+	{
+		EXPECT_TRUE(true);
+	}
+};
+
+namespace TiredBeans
+{
+	TEST(ExampleTests, Test3)
+	{
+		EXPECT_EQ(1, 1);
+	}
+};
