6ababd7e2d931fa2d8c062bc1d2a42428f70e3b7
Dylan <ricedylan3@gmail.com>
2021-01-03 19:38:12 -0800

    Revert "Fixed Google Tests"
    This reverts commit 8843fad131c4ecf3942ca9561cf1367158ce3373.

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,24 +1,8 @@
 #include "gtest/gtest.h"
 
-namespace Snoodle
+TEST(blaTest, test1)
 {
-	TEST(ExampleTests, test1)
-	{
-		EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
 
-		EXPECT_EQ(17, 17);
-	}
-
-	TEST(ExampleTest2, test2)
-	{
-		EXPECT_TRUE(true);
-	}
-};
-
-namespace TiredBeans
-{
-	TEST(ExampleTests, Test3)
-	{
-		EXPECT_EQ(1, 1);
-	}
-};
+	EXPECT_EQ(1, 1);
+}
