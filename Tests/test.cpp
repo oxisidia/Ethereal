7eda18322bc9d8eff9ee902b6a3c53aa989da298
Dylan <ricedylan3@gmail.com>
2021-01-04 21:37:17 -0800

    Added Eigen library and varified it's basic functionality
    From the research I've done Eigen seems to be a strong first choice for handling matrix and possibly vector math for Ethereal. It boasts very fast compute times compared with most other libraries.

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,8 +1,70 @@
 #include "gtest/gtest.h"
 
-TEST(PointTests, test1)
+namespace PointTests
 {
-	EXPECT_EQ(1, 1);
+	namespace SingleThreaded
+	{
+		TEST(PointTests, Creation)
+		{
+			EXPECT_EQ(1, 1);
+		}
 
-	EXPECT_EQ(1, 1);
-}
+		TEST(PointTests, Copy)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, Distance)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, Translation)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, Transposition)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, Deletion)
+		{
+			EXPECT_EQ(1, 1);
+		}
+	};
+
+	namespace MultiThreaded
+	{
+		TEST(PointTests, CreationMulti)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, CopyMulti)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, DistanceMulti)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, TranslationMulti)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, TranspositionMulti)
+		{
+			EXPECT_EQ(1, 1);
+		}
+
+		TEST(PointTests, DeletionMulti)
+		{
+			EXPECT_EQ(1, 1);
+		}
+	};
+};
